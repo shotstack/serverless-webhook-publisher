3d70e33f7fa231b89876e1b4535f8b31f3261cf0
Jeff Shillitto <jeff@shillitto.com>
2020-02-11 21:27:02 +1100

    Add timeout and error handling

--- src/worker.js
+++ src/worker.js
@@ -3,8 +3,8 @@
 const axios = require('axios');
 const retry = require('./lib/retry');
 
-// Todo:
-// Timeout/Requeue if no response within 15 seconds
+const RESPONSE_TIMEOUT_AFTER_MS = 10000;
+
 module.exports.consume = async event => {
     const messageData = JSON.parse(event.Records[0].body);
     const messageId = event.Records[0].messageId;
@@ -19,7 +19,8 @@ module.exports.consume = async event => {
             ['X-' + headerName + '-Attempt']: messageData.attempt || 1,
             'User-Agent': headerName + '-Webhook/1.0',
             'Content-Type': 'application/json'
-        }
+        },
+        timeout: RESPONSE_TIMEOUT_AFTER_MS
     }
 
     try {
@@ -36,7 +37,13 @@ module.exports.consume = async event => {
 
         console.log('Callback successfully delivered.')
     } catch (error) {
-        console.error('Callback POST failed with status code: '  + error.response.status);
+        if (error.code && error.code === 'ECONNABORTED') {
+            console.error('Response exceeded timeout of : '  + RESPONSE_TIMEOUT_AFTER_MS + 'ms');
+        }
+
+        if (error.response && error.response.status) {
+            console.error('Callback POST failed with status code: '  + error.response.status);
+        }
 
         try {
             const requeued = await retry.requeue(messageData, messageId);
